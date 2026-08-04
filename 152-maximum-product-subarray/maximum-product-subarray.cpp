6c6983268ece6fed8738bce93ffdd534ed49f18d
Soumya Jain <151627938+soumya-jain123@users.noreply.github.com>
2026-08-04 20:20:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (5.89%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,19 +3,19 @@ public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> maxDp(n);
-        vector<int> minDp(n);
+        vector<int> maxPro(n);
+        vector<int> minPro(n);
 
-        maxDp[0] = nums[0];
-        minDp[0] = nums[0];
+        maxPro[0] = nums[0];
+        minPro[0] = nums[0];
 
         int ans = nums[0];
 
         for(int i = 1; i < n; i++){
-            maxDp[i] = max({nums[i], nums[i] * maxDp[i - 1], nums[i] * minDp[i - 1]});
-            minDp[i] = min({nums[i], nums[i] * minDp[i - 1], nums[i] * maxDp[i - 1]});
+            maxPro[i] = max({nums[i], nums[i] * maxPro[i - 1], nums[i] * minPro[i - 1]});
+            minPro[i] = min({nums[i], nums[i] * minPro[i - 1], nums[i] * maxPro[i - 1]});
 
-            ans = max(ans, maxDp[i]);
+            ans = max(ans, maxPro[i]);
         }
 
         return ans;
